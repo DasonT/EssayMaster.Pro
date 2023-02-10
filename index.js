53750cf0d8c62bf047805e6267c0bc21d0fcaeb2
DasonT <115427130+DasonT@users.noreply.github.com>
2023-02-10 14:25:32 +0000

    Updated files

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-
 const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
